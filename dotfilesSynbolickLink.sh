495b05decfa7cdb7cb8df1577bd739ffc2c3257e
drumnistnakano <syxqh349ysykn@gmail.com>
2021-03-22 00:56:28 +0900

    oh-my-zshの削除

--- dotfilesSynbolickLink.sh
+++ dotfilesSynbolickLink.sh
@@ -4,4 +4,3 @@ ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/rc ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
