31cfd5da64bae5300737b5b1d6e2b9d5120a3f3b
drumnistnakano <syxqh349ysykn@gmail.com>
2023-08-27 23:19:04 +0900

    add brewfile script for synboliclink

--- dotfilesSynbolickLink.sh
+++ dotfilesSynbolickLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/rc ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.Brewfile ~/.Brewfile
